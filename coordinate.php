5f472e902df1f7c18d8e4f62d7b9514e84d6f43f
JackLiu <ceophp@163.com>
2016-06-02 20:41:25 +0800

    Update coordinate.php

--- coordinate.php
+++ coordinate.php
@@ -1,4 +1,5 @@
 <?php
+//测试代码
 $data = [2,3,2,4,7,1,4,7,8,2];
 $result = coordinate($data);
 print_r($result);
@@ -10,13 +11,13 @@ exit;
  * Email: ceophp@163.com
  * 
  * $y_downline Y轴谷值
- * $y_online   Y轴峰值
+ * $y_upline   Y轴峰值
  * 备注：请在PHP版本不小于5.4.0版本下运行！
  * 示例:[2, 1],[5, 1],[5, 4],[7, 4],[7, 1],[11, 1],[11, 4],[18, 4],
  * [18, 1],[19, 1],[19, 4],[23, 4],[23, 1],[30, 1],[30, 4],[38, 4],
  * [38, 1],[40, 1],[40, 4]
  */
-function coordinate($points, $y_downline = 1, $y_online = 4){
+function coordinate($points, $y_downline = 1, $y_upline = 4){
 	$y_axises = [];//Y值系初始化
 	$count = count($points);//统计用于生成Y轴匹配点组数量
 	//生成波峰和峰谷对应的Y值（两个为一组，偶数组为谷值, 奇数组为峰值）
@@ -25,8 +26,8 @@ function coordinate($points, $y_downline = 1, $y_online = 4){
 			$y_axises[] = $y_downline;
 			$y_axises[] = $y_downline;
 		}else{
-			$y_axises[] = $y_online;
-			$y_axises[] = $y_online;
+			$y_axises[] = $y_upline;
+			$y_axises[] = $y_upline;
 		}
 	}
 	
